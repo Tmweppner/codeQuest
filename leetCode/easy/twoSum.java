0e563f6b9ff014c28c009ffd66a2fa8d21230f0e
Tmweppner <tmweppne@buffalo.edu>
2017-07-07 05:40:17 -1100

    completed, but not fast

--- leetCode/easy/twoSum.java
+++ leetCode/easy/twoSum.java
@@ -8,56 +8,24 @@
 //
 // Because nums[0] + nums[1] = 2 + 7 = 9,
 // return [0, 1].
-//DOESNT WORK
-
-import java.util.PriorityQueue;
+//WORKS
+//speedup idea:
+//sort and have i start at largest and j start at shortest ...
 public class Solution {
     public int[] twoSum(int[] nums, int target) {
         int[] ans = new int[2];
-        PriorityQueue<Integer> sorted = new PriorityQueue<Integer>();
-        for(int i = 0; i<nums.length; i++){
-//            if(nums[i] <= target){
-                sorted.add(nums[i]);
-//            }
-        }
-        if(sorted.size() == 2){
-            int first = indexInNums(sorted.poll(), nums);
-            nums[first] = Integer.MAX_VALUE;
-            int second = indexInNums(sorted.poll(), nums);
-            ans[0] = first;
-            ans[1] = second;
-            return ans;
-        }
-        else{
-            Integer[] sortArr = sorted.toArray(new Integer[sorted.size()]);
-            int len = sortArr.length;
-            for(int i = 0; i<len; i++){
-                Integer largest = sortArr[len-1];
-                sorted.remove(largest);
-                Integer looking = target-largest;
-                if(sorted.contains(looking)){
-                    ans[0] = indexInNums(looking, nums);
-                    nums[indexInNums(looking, nums)] = Integer.MAX_VALUE;
-                    ans[1] = indexInNums(largest, nums);
+        int left = 0;
+        
+        for(int i = 0; i < nums.length - 1; i++){
+            for(int j = i+1; j < nums.length; j++){
+                if(nums[i] + nums[j] == target){
+                    ans[0] = i;
+                    ans[1] = j;
                     return ans;
-                }
-                else{
-
-                    len--;
-                }
+                }   
             }
         }
-        return ans;
+        
+        return null;
     }
-    public int indexInNums(int ans, int[] nums){
-        for(int i = 0; i < nums.length; i++){
-            if(ans == nums[i]){
-                return i;
-            }
-        }
-        return -1;
-
-    }
-
-
 }
