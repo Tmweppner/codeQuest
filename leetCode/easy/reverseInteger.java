b1e4365da83b1e467e083642669781524c226540
Tmweppner <tmweppne@buffalo.edu>
2017-07-07 06:08:20 -1100

    doesnt work, but im close

--- leetCode/easy/reverseInteger.java
+++ leetCode/easy/reverseInteger.java
@@ -1,5 +1,26 @@
 public class Solution {
     public int reverse(int x) {
+        int negative = 0;
         
+        if( x < 0 ){
+            int original = x;
+            negative = 1;
+            x -= original;
+            x -= original;
+        }
+        if(x == 0){
+            return 0;
+        }
+        
+        String s = Integer.toString(x);
+        StringBuilder rs = new StringBuilder(s).reverse();
+        if(negative == 1){
+            rs.insert(0, "-");
+        }
+        
+        
+        return Integer.parseInt(rs.toString());
+        
+       // return 0;
     }
 }
